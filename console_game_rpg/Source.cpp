68d3dd705af94d964112c1a4a25a4c2f971ca15c
mpalucki <mat.palucki@gmail.com>
2018-01-11 20:15:44 +0100

    time flow

--- console_game_rpg/Source.cpp
+++ console_game_rpg/Source.cpp
@@ -5,24 +5,37 @@
 
 using namespace std;
 
+string getTimeFromSlots(int slots) {
+	int hours = 10 + (slots / 4);
+	string hours_str = to_string(hours);
+	int minutes = (slots % 4) * 15;
+	string minutes_str = to_string(minutes);
+	if (minutes == 0) {
+		minutes_str = "00";
+	}
+	string time = "" + hours_str + ":" + minutes_str;
+	return time;
+}
+
 int main()
 {
 	std::setlocale(LC_ALL, "pl-PL");
 	string input;
+	int usedSlots = 0;
 
 	cout << "Wybierz imiê dla swego gracza:\n";
 	cin >> input;
 	Student student = Student(input);
-	cout << "\n" << student.getName() << "...";
+	cout << student.getName() << "...";
 	cout << "\n" << student.getName() << "...";
 	cout << "\n" << student.getName() << "!!!\n";
 	cout << "Powoli otwierasz oczy. Piekielnie boli Ciê g³owa.\nRozgl¹dasz siê dooko³a. Le¿ysz na trawniku, przed akademikiem.\nTwój wspó³lokator widz¹c, ¿e uda³o mu siê Ciê dobudziæ, odwraca siê i odchodzi, mrucz¹c coœ pod nosem.\n";
 	cout << "'Dzieñ jak co dzieñ' - myœlisz. Jedyne co pamiêtasz to 3 rzeczy:\n1) masz oddaæ poœciel do prania i zap³aciæ za pranie\n2) masz zadzwoniæ do mamy, aby powiedzieæ jej, ¿e u Ciebie wszystko w porz¹dku\n3) najwa¿niejsze - dziœ o 14:00 masz egzamin, a zaraz po nim musisz biec na poci¹g do domu.\n";
-	cout << "Jest godzina 9:00. W portfelu 10z³. Dramat...\n\n";
+	cout << "Jest godzina 10:00. W portfelu 10z³. Dramat...\n\n";
 	cout << "********** GRA ROZPOCZÊTA! ***********\n\n";
-	while (true)
+	while (usedSlots < 16)
 	{
-		cout << "Jest godzina 9:00\n";
+		cout << "Jest godzina " << getTimeFromSlots(usedSlots) << "\n";
 		cout << "Co zamierzasz teraz zrobiæ ?\n";
 		cout << "1) Iœæ do sklepu\n";
 		cout << "2) Zadzwoniæ do mamy\n";
@@ -33,6 +46,40 @@ int main()
 		cout << "6) Iœæ do swojego pokoju w akademiku\n";
 		cout << "Wpisz numer akcji: ";
 		cin >> input;
+		if (input == "1") 
+		{
+
+			usedSlots++;
+		}
+		else if (input == "2") 
+		{
+
+			usedSlots++;
+		}
+		else if (input == "3")
+		{
+
+			usedSlots++;
+		}
+		else if (input == "4")
+		{
+
+			usedSlots++;
+		}
+		else if (input == "5")
+		{
+
+			usedSlots++;
+		}
+		else if (input == "6")
+		{
+
+			usedSlots++;
+		}
+		else
+		{
+			cout << "Nieprawid³owy numer...\n";
+		}
 	}
 	return 0;
 }
